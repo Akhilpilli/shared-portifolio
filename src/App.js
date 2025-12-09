9b232f30b2b07353964fc13fd02a0e11c05793e5
Akhil_Pilli <31791605+Akhilpilli@users.noreply.github.com>
2025-12-09 15:01:51 -0600

    Add Theme based toggle (#1)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ThemeProvider } from "./contexts/ThemeContext";
 import Navbar from "./components/Navbar";
+import ThemeToggle from "./components/ThemeToggle";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import ProfessionalTimeline from "./components/ProfessionalTimeline";
@@ -15,13 +17,20 @@ const App = () => {
   useEffect(() => {
     const handleScroll = () => {
       const navbar = document.getElementById("navbar");
+      const themeToggle = document.querySelector(".floating-theme-toggle");
       const fullscreenDivHeight =
         document.querySelector(".about-description").offsetHeight;
 
       if (window.scrollY > fullscreenDivHeight) {
         navbar.style.display = "flex"; // Show the navbar when scrolled beyond the fullscreen div
+        if (themeToggle) {
+          themeToggle.style.display = "none"; // Hide floating toggle when navbar is visible
+        }
       } else {
         navbar.style.display = "none"; // Hide the navbar when within the fullscreen div
+        if (themeToggle) {
+          themeToggle.style.display = "flex"; // Show floating toggle when at top
+        }
       }
     };
 
@@ -32,19 +41,22 @@ const App = () => {
     };
   }, []);
   return (
-    <Router>
-      <section id="about">
-        <About config={config} />
-      </section>
-      <Navbar />
-      <section id="experience">
-        <ProfessionalTimeline  config={config} />
-      </section>
-      <section id="contact">
-        <Contact config={config} />
-      </section>
-      <Footer config={config} />
-    </Router>
+    <ThemeProvider>
+      <Router>
+        <ThemeToggle />
+        <section id="about">
+          <About config={config} />
+        </section>
+        <Navbar />
+        <section id="experience">
+          <ProfessionalTimeline  config={config} />
+        </section>
+        <section id="contact">
+          <Contact config={config} />
+        </section>
+        <Footer config={config} />
+      </Router>
+    </ThemeProvider>
   );
 };
 
